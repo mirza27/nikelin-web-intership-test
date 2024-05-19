f5d4a61745af5510d62ff79ff0f9b1b941b9b4aa
mirza27 <ramadhanimirza10@gmail.com>
2024-05-19 23:10:29 +0700

    update max runtime patch reqeust

--- src/app/api/approval/route.ts
+++ src/app/api/approval/route.ts
@@ -1,3 +1,6 @@
+export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma";
 import { getSession } from "@/app/lib/session";
